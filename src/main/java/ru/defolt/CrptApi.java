10c4dcffb4f8510d8c961796a08fc4952afc8700
Alex <lipilinalexey@gmail.com>
2024-04-20 19:40:58 +0300

    init

--- src/main/java/ru/defolt/CrptApi.java
+++ src/main/java/ru/defolt/CrptApi.java
@@ -20,7 +20,6 @@ import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class CrptApi {
